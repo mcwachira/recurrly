b0310809bcb7623d18b7141e44b27940ce29ded8
mcwachira <mcwachira@outlook.com>
2026-07-17 13:44:13 +0300

    Set up the react native up using expo and configured native wind for styling.

---
+++ app/onboarding.tsx
@@ -0,0 +1,14 @@
+import {View , Text} from 'react-native';
+import React from 'react';
+
+const Onboarding = () => {
+    return(
+        <View>
+            <Text>
+                Onboardings
+            </Text>
+        </View>
+    )
+}
+
+export default Onboarding;
